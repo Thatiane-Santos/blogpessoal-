1d81ae65e918812dc50f1eca35a93b3bdc49256a
Thatiane <thatiane.ssantana@gmail.com>
2025-08-08 10:31:21 -0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,26 +9,26 @@ import { AuthModule } from "./auth/auth.module";
 import { Usuario } from "./usuario/entities/usuario.entity";
 import { UsuarioModule } from "./usuario/usuario.module";
 
+    
+  @Module({
+    imports: [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '250319',
+        database: 'db_blogpessoal',
+        entities: [Postagem, Tema, Usuario],
+        synchronize: true,
 
-@Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '250319',
-      database: 'db_blogpessoal',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
-
-    }),  
-    PostagemModule,
-    TemaModule,
-    AuthModule,
-    UsuarioModule
-  ],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
+      }),  
+      PostagemModule,
+      TemaModule,
+      AuthModule,
+      UsuarioModule
+    ],
+    controllers: [],
+    providers: [],
+  })
+  export class AppModule {}
