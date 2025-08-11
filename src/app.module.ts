6b45c13197085044f38e2b4ec01913669cf3f67b
Thatiane <thatiane.ssantana@gmail.com>
2025-08-11 12:51:23 -0300

    2 - Deploy do Blogpessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { DevService } from "./data/services/dev.service";
       
       ConfigModule.forRoot(),
 TypeOrmModule.forRootAsync({
-	useClass: DevService,
+	useClass: ProdService,
     imports: [ConfigModule],
 
 }),
